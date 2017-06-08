1d25ced421e001510eaba7bbc73906388d5eaf83
lichuanjun <lichuanjun@niwodai.net>
2017-06-08 22:37:13 +0800

    文件结构部署，设置启动文件

---
+++ TLWeibo/Class/View/Home/TLHomeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TLHomeViewController.swift
+//  TLWeibo
+//
+//  Created by lichuanjun on 2017/6/8.
+//  Copyright © 2017年 lichuanjun. All rights reserved.
+//
+
+import UIKit
+
+class TLHomeViewController: TLBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
